c07e2787307dbedc4d66ecca2f7c3a856303b614
Danilo Setra <danilo.setra@rga.com>
2018-07-11 19:54:48 -0300

    default propTypes maintained

--- src/js/Spritesheet.js
+++ src/js/Spritesheet.js
@@ -24,7 +24,41 @@ class Spritesheet extends React.Component {
 }
 
 Spritesheet.propTypes = {
-  className: PropTypes.string
+  className: PropTypes.string,
+  style: PropTypes.object,
+  image: PropTypes.string.isRequired,
+  widthFrame: PropTypes.number.isRequired,
+  heightFrame: PropTypes.number.isRequired,
+  isResponsive: PropTypes.bool,
+  steps: PropTypes.number.isRequired,
+  fps: PropTypes.number.isRequired,
+  direction: PropTypes.string,
+  timeout: PropTypes.number,
+  autoplay: PropTypes.bool,
+  loop: PropTypes.bool,
+  startAt: PropTypes.number,
+  endAt: PropTypes.number,
+  background: PropTypes.string,
+  backgroundSize: PropTypes.string,
+  backgroundRepeat: PropTypes.string,
+  backgroundPosition: PropTypes.string,
+  getInstance: PropTypes.func,
+  onClick: PropTypes.func,
+  onDoubleClick: PropTypes.func,
+  onMouseMove: PropTypes.func,
+  onMouseEnter: PropTypes.func,
+  onMouseLeave: PropTypes.func,
+  onMouseOver: PropTypes.func,
+  onMouseOut: PropTypes.func,
+  onMouseDown: PropTypes.func,
+  onMouseUp: PropTypes.func,
+  onInit: PropTypes.func,
+  onResize: PropTypes.func,
+  onPlay: PropTypes.func,
+  onPause: PropTypes.func,
+  onLoopComplete: PropTypes.func,
+  onEachFrame: PropTypes.func,
+  onEnterFrame: PropTypes.array
 };
 
 export default Spritesheet;
